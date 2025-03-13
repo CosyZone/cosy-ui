21dbfe61ad67b7b87932fb17de695b6ae58b026c
Nookery <nooks@qq.com>
2025-03-13 23:57:58 +0800

    🚀 perf: improve loading performance

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
+
+export { default as Banner } from './src/components/Banner.astro';
+export { default as Footer } from './src/components/Footer.astro';
 export { default as Header } from './src/components/Header.astro';
 export { default as Hero } from './src/components/Hero.astro';
-export { default as Footer } from './src/components/Footer.astro';
-export { default as Banner } from './src/components/Banner.astro';
 export { default as Link } from './src/components/Link.astro';
 export { default as SocialIcon } from './src/components/SocialIcon.astro';
-export { default as Component } from './src/components/Component.astro';
 export { default as ThemeItem } from './src/components/ThemeItem.astro';
