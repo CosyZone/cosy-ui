150b7720fe7f5fdc6a1c840ee5b7ed28f39b0bca
Nookery <nooks@qq.com>
2025-03-13 13:02:49 +0800

    🔨 build: upgrade build system to latest version

---
+++ index.ts
@@ -0,0 +1,7 @@
+// @ts-expect-error
+export { Footer } from './src/components/Footer.astro';
+
+// @ts-expect-error
+export { Component } from './src/components/Component.astro';
+
+export type { FooterProps } from './src/main';
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './components';
-export * from './themes';
-export * from './components/Footer/types'; 
