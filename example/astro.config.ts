65e962c0d55b4567a57a7d1a19fef68fdff20d2c
Nookery <nooks@qq.com>
2025-03-14 11:23:16 +0800

    🔧 chore: update development configuration

--- example/astro.config.ts
+++ example/astro.config.ts
@@ -4,6 +4,10 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+	// 设置基础路径为仓库名，这里使用 cosy-ui
+	base: '/cosy-ui',
+	// 设置输出模式为静态网站
+	output: 'static',
 	integrations: [vue()],
 	vite: {
 		// @ts-ignore
