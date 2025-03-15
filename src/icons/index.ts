2b11ba7254c8deb3536af5741e398568b5a293a4
Nookery <nooks@qq.com>
2025-03-15 11:36:34 +0800

    🎨 style: improve button and layout styles

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -3,4 +3,11 @@ export { default as AlertTriangle } from './AlertTriangle.astro';
 export { default as CheckCircle } from './CheckCircle.astro';
 export { default as XCircle } from './XCircle.astro';
 export { default as InfoCircle } from './InfoCircle.astro';
+export { default as InfoIcon } from './InfoIcon.astro';
+export { default as SuccessIcon } from './SuccessIcon.astro';
+export { default as WarningIcon } from './WarningIcon.astro';
+export { default as ErrorIcon } from './ErrorIcon.astro';
+export { default as CloseIcon } from './CloseIcon.astro';
+export { default as ClipboardIcon } from './ClipboardIcon.astro';
+export { default as CheckIcon } from './CheckIcon.astro';
 // 随着图标增加，可以在这里继续添加导出 
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+// 这里用于导出所有组件，注意按照字母排序
+export { default as Alert } from './components/Alert.astro';
+export { default as Article } from './components/Article.astro';
+export { default as Blog } from './components/Blog.astro';
+export { default as Banner } from './components/Banner.astro';
+export { default as Button } from './components/Button.astro';
+export { default as Card } from './components/Card.astro';
+export { default as CodeBlock } from './components/CodeBlock.astro';
+export { default as CodeExample } from './components/CodeExample.astro';
+export { default as Footer } from './components/Footer.astro';
+export { default as Header } from './components/Header.astro';
+export { default as Hero } from './components/Hero.astro';
+export { default as Image } from './components/Image.astro';
+export { default as Link } from './components/Link.astro';
+export { default as Modal } from './components/Modal.astro';
+export { default as SocialIcon } from './components/SocialIcon.astro';
+export { default as TableOfContents } from './components/TableOfContents.astro';
+export { default as ThemeItem } from './components/ThemeItem.astro';
+export { default as TeamMember } from './components/TeamMember.astro';
+export { default as TeamMembers } from './components/TeamMembers.astro';
+
+// 导出所有图标
+export * from './icons'; 
