9ed2ca25ff78acf2ea748bdb8963b1cdd211945b
Nookery <nooks@qq.com>
2025-03-16 23:05:21 +0800

    ✨ feat: implement new user interface

--- demo/astro.config.mts
+++ demo/astro.config.mts
@@ -1,15 +1,6 @@
-import { createResolver } from "astro-integration-kit";
-import { hmrIntegration } from "astro-integration-kit/dev";
 import { defineConfig } from "astro/config";
-import { integration } from "@coffic/cosy-ui";
 
 // https://astro.build/config
 export default defineConfig({
 	base: '/cosy-ui/',
-	integrations: [
-		integration(),
-		// hmrIntegration({
-		// 	directory: createResolver(import.meta.url).resolve("../packages/cosy-ui/src"),
-		// }),
-	],
 });
--- packages/cosy-ui/src/index.ts
+++ packages/cosy-ui/src/index.ts
@@ -1,9 +1,9 @@
-export * from "./integration.js";
 export * from './components/base';
-// export * from './components/navigation';
-// export * from './components/display';
-// export * from './components/data-display';
-// export * from './components/layouts';
-// export * from './components/typography';
-// export * from './components/icons';
-// export * from './components/containers';
+export * from './components/navigation';
+export * from './components/display';
+export * from './components/data-display';
+export * from './components/layouts';
+export * from './components/typography';
+export * from './components/icons';
+export * from './components/containers';
+
