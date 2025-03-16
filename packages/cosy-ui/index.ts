e02133df96864517ff956ab92272e611bcd23af1
Nookery <nooks@qq.com>
2025-03-16 12:31:10 +0800

    🚀 perf: improve loading performance

--- packages/cosy-ui/index.ts
+++ packages/cosy-ui/index.ts
@@ -1,1 +1,23 @@
-export * from './src/main'; 
+// 基础组件
+export * from './src/components/base';
+
+// 导航组件
+export * from './src/components/navigation';
+
+// 展示组件
+export * from './src/components/display';
+
+// 数据展示组件
+export * from './src/components/data-display';
+
+// 布局组件
+export * from './src/components/layouts';
+
+// 排版组件
+export * from './src/components/typography';
+
+// 图标组件
+export * from './src/components/icons';
+
+// 容器组件
+export * from './src/components/containers';
