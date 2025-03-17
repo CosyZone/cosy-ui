4bd4f74518892cf1c3b72bf1e8145de231e0065c
Nookery <nooks@qq.com>
2025-03-17 14:33:18 +0800

    ✨ feat: implement new user interface

--- packages/cosy-ui/astro.config.ts
+++ packages/cosy-ui/astro.config.ts
@@ -7,20 +7,21 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     base: '/cosy-ui/',
     srcDir: 'docs',
+    outDir: './dist-docs',
     vite: {
-      resolve: {
-          alias: {
-              '@': path.resolve('./src'),
-              '@/*': path.resolve('./src/*'),
-              '@components': path.resolve('./src/components'),
-              '@utils': path.resolve('./src/utils'),
-              '@paths': path.resolve('./src/paths'),
-              '@database': path.resolve('./src/database'),
-              '@models': path.resolve('./src/models'),
-              '@layouts': path.resolve('./src/layouts'),
-          },
-      },
+        resolve: {
+            alias: {
+                '@': path.resolve('./src'),
+                '@/*': path.resolve('./src/*'),
+                '@components': path.resolve('./src/components'),
+                '@utils': path.resolve('./src/utils'),
+                '@paths': path.resolve('./src/paths'),
+                '@database': path.resolve('./src/database'),
+                '@models': path.resolve('./src/models'),
+                '@layouts': path.resolve('./src/layouts'),
+            },
+        },
 
-      plugins: [tailwindcss()],
+        plugins: [tailwindcss()],
     },
 });
