042199e0d81b877085c2e3643a7e19bef8c7ff5d
Nookery <nooks@qq.com>
2025-03-17 09:44:50 +0800

    🔄 refactor: optimize code structure

--- packages/cosy-ui/vite.config.ts
+++ packages/cosy-ui/vite.config.ts
@@ -2,5 +2,17 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-	plugins: [tailwindcss()],
+	plugins: [
+		// @ts-ignore
+		tailwindcss()
+	],
+	build: {
+		cssCodeSplit: false,
+		rollupOptions: {
+			input: 'src/style.ts',
+			output: {
+				assetFileNames: 'app.css'
+			}
+		}
+	}
 });
