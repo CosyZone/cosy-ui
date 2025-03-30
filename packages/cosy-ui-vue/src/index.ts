7514a93f0c023f8bae11e52853004e4de2869a29
Nookery <nooks@qq.com>
2025-03-30 20:41:12 +0800

    2 modified, 1 deleted

--- packages/cosy-ui-vue/src/index.ts
+++ packages/cosy-ui-vue/src/index.ts
@@ -1,27 +1,7 @@
-import type { App } from 'vue'
+
 import Button from './components/Button.vue'
 import './style.css'
 
-// 组件列表
-const components = [
-    Button,
-]
-
-// 定义安装方法
-const install = (app: App): void => {
-    // 注册组件
-    components.forEach(component => {
-        if (component.name) {
-            app.component(component.name, component)
-        }
-    })
-}
-
 export {
     Button as CosyButton,
-}
-
-// 导出默认对象
-export default {
-    install
 }
--- packages/docs-vue/src/App.vue
+++ packages/docs-vue/src/App.vue
@@ -3,5 +3,6 @@ import { CosyButton } from '@coffic/cosy-ui-vue'
 </script>
 
 <template>
+    <h1>Cosy UI Vue Documents</h1>
     <CosyButton>Button</CosyButton>
 </template>
