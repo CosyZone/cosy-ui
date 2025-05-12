c0354b2814938ad231d66192f64037069ca28273
nookery <nooks@foxmail.com>
2025-05-12 05:06:26 +0000

    refactor(docs): update navigation links and restructure course guide
    This commit modifies the app configuration to update the course link in the sidebar navigation. The title of the course guide is changed to "Guide" for clarity. Additionally, the navigation structure is enhanced by introducing a new NavItems component, which centralizes the rendering of navigation items in the header layout, improving maintainability and readability.

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -3,12 +3,12 @@ import { LinkUtil, type INavItem } from '@/index';
 export const homeLink = LinkUtil.getBaseUrl();
 export const basePath = LinkUtil.getBaseUrl();
 export const navItems: INavItem[] = [
+	// {
+	// 	href: homeLink,
+	// 	label: 'Home',
+	// },
 	{
-		href: homeLink,
-		label: 'Home',
-	},
-	{
-		href: `${basePath}en/courses/guide`,
+		href: `${basePath}en/courses`,
 		label: 'Docs',
 	},
 	{
