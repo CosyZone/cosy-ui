fb34e40ebf11108c03c30f32fce95784551ef84a
nookery <nooks@foxmail.com>
2025-06-04 18:43:57 +0800

    🎨 Chore:  Minor adjustments

--- src/index_vue.ts
+++ src/index_vue.ts
@@ -1,44 +1,33 @@
 // AlertDialog
-export { AlertDialogExamples, AlertDialogExampleCodes, AlertDialog } from './vue/AlertDialog';
+export * from './alert-dialog-vue/index';
 
 // Banner
-export { BannerBoxExamples, BannerBoxExampleCodes, BannerBox } from './vue/BannerBox';
+export * from './banner-box-vue/index';
 
 // BlogList
-export * from './vue/BlogList';
+export * from './blog-vue/index';
 
 // Counter
-export { default as VueCounter } from './vue/VueCounter.vue';
+export * from './counter-vue/index';
 
 // ConfirmDialog
-export { default as ConfirmDialog } from './vue/ConfirmDialog/ConfirmDialog.vue';
-export { ConfirmDialogExamples, ConfirmDialogExampleCodes } from './vue/ConfirmDialog';
+export * from './confirm-dialog-vue/index';
 
 // Buttons
-export * from './button/index_vue';
-
-export { default as FeatureCard } from './vue/BannerBox/FeatureCard.vue';
+export * from './buttons-vue/index';
 
 // Icons
-export { default as SearchIcon } from './vue/Icons/SearchIcon.vue';
-export { default as SettingsIcon } from './vue/Icons/SettingsIcon.vue';
-export { default as InfoIcon } from './vue/Icons/InfoIcon.vue';
-export { default as InfoCircleIcon } from './vue/Icons/InfoCircleIcon.vue';
-export { default as LinkIcon } from './vue/Icons/LinkIcon.vue';
-export { default as MenuIcon } from './vue/Icons/MenuIcon.vue';
-export { default as UserIcon } from './vue/Icons/UserIcon.vue';
-export { default as XCircleIcon } from './vue/Icons/XCircleIcon.vue';
-export { default as InboxArchiveIcon } from './vue/Icons/InboxArchiveIcon.vue';
+export * from './icons-vue/index';
 
 // List
-export { default as ListItem } from './vue/ListItem.vue';
+export * from './list-vue/index';
 
 // Windows
-export { MacWindowExamples, MacWindowExampleCodes, MacWindow } from './vue/MacWindow';
-export { iPhoneWindowExamples, iPhoneWindowExampleCodes, iPhoneWindow } from './vue/iPhone';
+// export { MacWindowExamples, MacWindowExampleCodes, MacWindow } from './vue/MacWindow';
+// export { iPhoneWindowExamples, iPhoneWindowExampleCodes, iPhoneWindow } from './vue/iPhone';
 
 // SmartLink
-export { default as SmartLink } from './vue/SmartLink.vue';
+// export { default as SmartLink } from './vue/SmartLink.vue';
 
 // TagList
-export { default as TagList } from './vue/TagList.vue';
+// export { default as TagList } from './vue/TagList.vue';
