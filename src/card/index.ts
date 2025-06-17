e25975293450b37b47bfb36e3f1117561d83dfe0
nookery <nooks@qq.com>
2025-06-17 11:20:23 +0800

    🆕 Feature:  New card component

--- src/card/index.ts
+++ src/card/index.ts
@@ -1,7 +1,33 @@
+import Card from './Card.astro';
+import CardCourse from './CardCourse.astro';
+import CardBasic from './CardBasic.astro';
+import CardWithImage from './CardWithImage.astro';
+import CardLink from './CardLink.astro';
+import CardCompact from './CardCompact.astro';
 
-export { default as Card } from './Card.astro';
-export { default as CardCourse } from './CardCourse.astro';
-export { default as CardBasic } from './CardBasic.astro';
-export { default as CardWithImage } from './CardWithImage.astro';
-export { default as CardLink } from './CardLink.astro';
-export { default as CardCompact } from './CardCompact.astro';
+// Container components
+import CardBasicContainer from './ECardBasicContainer.astro';
+import CardWithSubtitleContainer from './ECardWithSubtitleContainer.astro';
+import CardWithImageContainer from './ECardWithImageContainer.astro';
+import CardClickableContainer from './ECardClickableContainer.astro';
+import CardCompactContainer from './ECardCompactContainer.astro';
+import CardCustomStyleContainer from './ECardCustomStyleContainer.astro';
+
+export { Card };
+export { CardCourse };
+export { CardBasic };
+export { CardWithImage };
+export { CardLink };
+export { CardCompact };
+
+export const CardPackage = {
+    Card,
+    CardContainers: {
+        Basic: CardBasicContainer,
+        WithSubtitle: CardWithSubtitleContainer,
+        WithImage: CardWithImageContainer,
+        Clickable: CardClickableContainer,
+        Compact: CardCompactContainer,
+        CustomStyle: CardCustomStyleContainer,
+    },
+};
