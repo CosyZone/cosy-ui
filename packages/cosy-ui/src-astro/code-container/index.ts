b3694184ea868b699ce44a213ca0c911badc9472
Nookery <nooks@qq.com>
2025-06-21 16:43:52 +0800

    🐎 Improve:  Enhance code container

--- packages/cosy-ui/src-astro/code-container/index.ts
+++ packages/cosy-ui/src-astro/code-container/index.ts
@@ -1,14 +1,38 @@
 import { default as CodeContainer } from './CodeContainer.astro';
 import { default as CodeContainerBasic } from './ECodeContainerBasic.astro';
 import { default as CodeContainerMultiple } from './ECodeContainerMultiple.astro';
+import { default as CodeContainerIsolation } from './ECodeContainerIsolation.astro';
+import { default as ECodeContainerBasicContainer } from './ECodeContainerBasicContainer.astro';
+import { default as ECodeContainerMultipleContainer } from './ECodeContainerMultipleContainer.astro';
+import { default as ECodeContainerIsolationContainer } from './ECodeContainerIsolationContainer.astro';
 import BasicSourceCode from './ECodeContainerBasic.astro?raw';
 import MultipleSourceCode from './ECodeContainerMultiple.astro?raw';
+import IsolationSourceCode from './ECodeContainerIsolation.astro?raw';
 import { extractSimpleExample } from '../utils/component';
 
-export { CodeContainer, CodeContainerBasic, CodeContainerMultiple };
+export {
+  CodeContainer,
+  CodeContainerBasic,
+  CodeContainerMultiple,
+  CodeContainerIsolation,
+  ECodeContainerBasicContainer,
+  ECodeContainerMultipleContainer,
+  ECodeContainerIsolationContainer
+};
 
 // 导出示例源代码
 export const CodeContainerExampleCodes = {
   Basic: extractSimpleExample(BasicSourceCode, 'CodeContainer'),
   Multiple: extractSimpleExample(MultipleSourceCode, 'CodeContainer'),
+  Isolation: extractSimpleExample(IsolationSourceCode, 'CodeContainer'),
+};
+
+// 导出ComponentPackage
+export const CodeContainerPackage = {
+  CodeContainer,
+  CodeContainerContainers: {
+    Basic: ECodeContainerBasicContainer,
+    Multiple: ECodeContainerMultipleContainer,
+    Isolation: ECodeContainerIsolationContainer,
+  },
 };
