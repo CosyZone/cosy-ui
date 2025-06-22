138745f6fba5c2c5661f6f8380a729746cbb077c
Nookery <nooks@qq.com>
2025-06-22 23:28:04 +0800

    🎨 Chore:  New confirm dialog

--- packages/cosy-ui/index-astro.ts
+++ packages/cosy-ui/index-astro.ts
@@ -43,6 +43,7 @@ export * from './src-astro/text';
 export * from './src-astro/theme-switcher';
 export * from './src-astro/toc';
 export * from './src-astro/toast';
+export * from './src-astro/confirm-dialog';
 
 // 类型定义 (按字母顺序)
 export * from './src-astro/types/article';
