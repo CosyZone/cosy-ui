308bf781833c6eabff8b71bb51690cd2355367b7
nookery <nooks@qq.com>
2025-07-04 10:58:50 +0800

    🆕 Feature: Add error, success, and warning icons to cosy-ui

--- packages/vue-example/src/App.vue
+++ packages/vue-example/src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import { Button, Container } from '@coffic/cosy-ui/vue';
+import { Alert, Button, Container } from '@coffic/cosy-ui/vue';
 </script>
 
 <template>
-  <Container border>
+  <Container border flex="col" gap="md">
     <Button>Click me</Button>
+
+    <Alert type="success">
+      <p>This is an alert</p>
+    </Alert>
+
+    <Alert type="error">
+      <p>This is an error alert</p>
+    </Alert>
+
+    <Alert type="warning">
+      <p>This is a warning alert</p>
+    </Alert>
   </Container>
 </template>
