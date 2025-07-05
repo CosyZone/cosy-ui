9b47a44ea35cb0de36ee4c0651f01965c1e12d0c
Nookery <nooks@qq.com>
2025-07-05 17:19:06 +0800

    🗑️ Trash: Remove deprecated iPhone component files

--- packages/cosy-ui/src-vue/iPhone/index.ts
+++ packages/cosy-ui/src-vue/iPhone/index.ts
@@ -1,27 +1,2 @@
-import Basic from './Basic.vue';
-import NoFrame from './NoFrame.vue';
-import CustomBackground from './CustomBackground.vue';
-import BasicSource from './Basic.vue?raw';
-import NoFrameSource from './NoFrame.vue?raw';
-import CustomBackgroundSource from './CustomBackground.vue?raw';
-import { extractSimpleExample } from '../utils/component';
-
 // 导出主组件
 export { default as iPhoneWindow } from './iPhoneWindow.vue';
-
-// 将示例组件整合为一个对象导出
-export const iPhoneWindowExamples = {
-    Basic,
-    NoFrame,
-    CustomBackground,
-};
-
-// 导出示例组件的源代码（简化版本）
-export const iPhoneWindowExampleCodes = {
-    Basic: extractSimpleExample(BasicSource, 'iPhoneWindow'),
-    NoFrame: extractSimpleExample(NoFrameSource, 'iPhoneWindow'),
-    CustomBackground: extractSimpleExample(
-        CustomBackgroundSource,
-        'iPhoneWindow'
-    ),
-};
