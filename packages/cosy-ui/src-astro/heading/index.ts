8c68a83301efbdf36b2fee98a55b24abe2ec14e0
Nookery <nooks@qq.com>
2025-07-06 15:23:03 +0800

    📖 Document: Update heading component docs and cleanup UI code

--- packages/cosy-ui/src-astro/heading/index.ts
+++ packages/cosy-ui/src-astro/heading/index.ts
@@ -1,11 +1,4 @@
 import Heading from './Heading.astro';
-import HeadingBasic from './HeadingBasic.astro';
-import BasicSourceCode from './HeadingBasic.astro?raw';
 import { extractSimpleExample } from '../../src/utils/component';
 
-export { Heading, HeadingBasic };
-
-// 导出示例源代码
-export const HeadingExampleCodes = {
-  Basic: extractSimpleExample(BasicSourceCode, 'Heading'),
-};
+export { Heading };
