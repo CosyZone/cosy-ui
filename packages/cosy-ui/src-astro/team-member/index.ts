b32d2a9a212a15186b63e65dc98ebf49ef97d727
nookery <nooks@qq.com>
2025-07-10 16:23:01 +0800

    🗑️ Trash: Remove deprecated team member components

--- packages/cosy-ui/src-astro/team-member/index.ts
+++ packages/cosy-ui/src-astro/team-member/index.ts
@@ -1,30 +1,7 @@
 import TeamMember from './TeamMember.astro';
-import TeamMemberBasic from './TeamMemberBasic.astro';
-import TeamMemberWithSocial from './TeamMemberWithSocial.astro';
-import TeamMemberCustomStyle from './TeamMemberCustomStyle.astro';
-import TeamMemberGroup from './TeamMemberGroup.astro';
-import TeamMemebrs from './TeamMembers.astro';
-
-import BasicSourceCode from './TeamMemberBasic.astro?raw';
-import WithSocialSourceCode from './TeamMemberWithSocial.astro?raw';
-import CustomStyleSourceCode from './TeamMemberCustomStyle.astro?raw';
-import GroupSourceCode from './TeamMemberGroup.astro?raw';
-
-import { extractSimpleExample } from '../../src/utils/component';
+import TeamMembers from './TeamMembers.astro';
 
 export {
-  TeamMember,
-  TeamMemberBasic,
-  TeamMemberWithSocial,
-  TeamMemberCustomStyle,
-  TeamMemberGroup,
-  TeamMemebrs,
-};
-
-// Export example codes
-export const TeamMemberExampleCodes = {
-  Basic: extractSimpleExample(BasicSourceCode, 'TeamMember'),
-  WithSocial: extractSimpleExample(WithSocialSourceCode, 'TeamMember'),
-  CustomStyle: extractSimpleExample(CustomStyleSourceCode, 'TeamMember'),
-  Group: extractSimpleExample(GroupSourceCode, 'TeamMember'),
+    TeamMember,
+    TeamMembers,
 };
