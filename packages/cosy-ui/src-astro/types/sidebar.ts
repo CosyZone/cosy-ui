4802b4e29f919bb28047323030905699e1618388
nookery <nooks@qq.com>
2025-07-11 18:34:42 +0800

    🆕 Feature: 更新文档组件及侧边栏相关功能

--- packages/cosy-ui/src-astro/types/sidebar.ts
+++ packages/cosy-ui/src-astro/types/sidebar.ts
@@ -2,35 +2,39 @@
  * 侧边栏项目
  */
 export interface ISidebarItem {
-  href: string;
-  text: string;
-  items?: ISidebarItem[];
+    href: string;
+    text: string;
+    items?: ISidebarItem[];
+    /**
+     * 可选徽标内容
+     */
+    badge?: string | number;
 }
 
 /**
  * 侧边栏配置
  */
 export interface ISidebarProps {
-  /**
-   * 侧边栏项目
-   */
-  sidebarItems: ISidebarItem[];
+    /**
+     * 侧边栏项目
+     */
+    sidebarItems: ISidebarItem[];
 
-  /**
-   * 桌面端类名
-   */
-  class?: string;
+    /**
+     * 桌面端类名
+     */
+    class?: string;
 
-  /**
-   * 是否开启调试模式，显示边框
-   * @default false
-   */
-  debug?: boolean;
+    /**
+     * 是否开启调试模式，显示边框
+     * @default false
+     */
+    debug?: boolean;
 
-  /**
-   * 侧边栏顶部外边距
-   */
-  marginTop?:
+    /**
+     * 侧边栏顶部外边距
+     */
+    marginTop?:
     | 'xs'
     | 'sm'
     | 'md'
@@ -42,10 +46,10 @@ export interface ISidebarProps {
     | '5xl'
     | string;
 
-  /**
-   * 侧边栏底部外边距
-   */
-  marginBottom?:
+    /**
+     * 侧边栏底部外边距
+     */
+    marginBottom?:
     | 'xs'
     | 'sm'
     | 'md'
