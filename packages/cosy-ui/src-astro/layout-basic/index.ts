944ca997941290f7e36a89f5236b9c823047a5b0
Nookery <nooks@qq.com>
2025-07-31 22:19:36 +0800

    🎨 Chore: Remove BaseLayoutBasic and update related components

--- packages/cosy-ui/src-astro/layout-basic/index.ts
+++ packages/cosy-ui/src-astro/layout-basic/index.ts
@@ -1,11 +1,3 @@
 import BaseLayout from './BaseLayout.astro';
-import BaseLayoutBasic from './BaseLayoutBasic.astro';
-import BasicSourceCode from './BaseLayoutBasic.astro?raw';
-import { extractSimpleExample } from '../../src/utils/component';
 
-export { BaseLayout, BaseLayoutBasic };
-
-// 导出示例源代码
-export const BaseLayoutExampleCodes = {
-  Basic: extractSimpleExample(BasicSourceCode, 'BaseLayout'),
-};
+export { BaseLayout };
