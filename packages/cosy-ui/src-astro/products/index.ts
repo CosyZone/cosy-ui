653e1152cfa589bb241c7ddcdfbef0a812f97d26
nookery <nooks@qq.com>
2025-08-04 09:02:52 +0800

    🆕 Feature: Add single column layout for products component

--- packages/cosy-ui/src-astro/products/index.ts
+++ packages/cosy-ui/src-astro/products/index.ts
@@ -1,2 +1,4 @@
 export { default as Products } from './Products.astro';
 export { default as ProductCard } from './ProductCard.astro';
+export type { Props as ProductsProps } from './Products.astro';
+export type { Props as ProductCardProps } from './ProductCard.astro';
