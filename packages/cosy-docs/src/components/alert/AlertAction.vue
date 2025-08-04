a716ca2cf9345f515b8f8adc50a589ae0a7d05d4
Nookery <nooks@qq.com>
2025-08-04 21:55:02 +0800

    🆕 Feature: Update alert component styles and documentation

--- packages/cosy-docs/src/components/alert/AlertAction.vue
+++ packages/cosy-docs/src/components/alert/AlertAction.vue
@@ -2,7 +2,15 @@
     <Alert type="info" title="自定义操作">
         你可以通过 action slot 在右侧自定义按钮。
         <template #action>
-            <button class="cosy:btn cosy:btn-primary" @click="handleAction">
+            <button style="
+                    background-color: #3b82f6;
+                    color: white;
+                    border: none;
+                    padding: 0.5rem 1rem;
+                    border-radius: 0.375rem;
+                    cursor: pointer;
+                    font-size: 0.875rem;
+                " @click="handleAction">
                 自定义操作
             </button>
         </template>
