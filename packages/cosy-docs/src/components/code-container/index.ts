ee68820a66b267c10eb3fc3d5c046042373d1315
nookery <nooks@qq.com>
2025-08-05 17:23:05 +0800

    🗑️ Trash: Remove deprecated code-container components and docs

--- packages/cosy-docs/src/components/code-container/index.ts
+++
@@ -1,9 +0,0 @@
-import Multiple from './Multiple.astro';
-import Basic from './Basic.astro';
-
-export const CodeContainerPackage = {
-    CodeContainerExamples: {
-        Basic: Basic,
-        Multiple: Multiple,
-    },
-}; 
