7b9f62215216a619be6ca8eaff7c0c88980fb32a
nookery <nooks@qq.com>
2025-08-06 10:54:40 +0800

    🆕 Feature: 更新Hero组件文档和实现

--- packages/cosy-docs/src/components/hero/index.ts
+++ packages/cosy-docs/src/components/hero/index.ts
@@ -1,16 +1,3 @@
-// Hero 示例组件
-import Basic from './Basic.astro';
-import AlignCenter from './AlignCenter.astro';
-import AlignLeft from './AlignLeft.astro';
-import AlignRight from './AlignRight.astro';
-import BackgroundImage from './BackgroundImage.astro';
-import GradientBackground from './GradientBackground.astro';
-import PlainBackground from './PlainBackground.astro';
-import WithButton from './WithButton.astro';
-import WithImage from './WithImage.astro';
-import WithImportedImage from './WithImportedImage.astro';
-import WithImportedBackground from './WithImportedBackground.astro';
-
 // Hero 容器组件
 import AlignContainer from './AlignContainer.astro';
 import BackgroundContainer from './BackgroundContainer.astro';
@@ -20,25 +7,10 @@ import WithButtonContainer from './WithButtonContainer.astro';
 import WithImageContainer from './WithImageContainer.astro';
 
 export const HeroPackage = {
-    HeroContainers: {
-        Basic: BasicContainer,
-        Align: AlignContainer,
-        Background: BackgroundContainer,
-        BackgroundImage: BackgroundImageContainer,
-        WithButton: WithButtonContainer,
-        WithImage: WithImageContainer,
-    },
-    HeroExamples: {
-        Basic: Basic,
-        AlignCenter: AlignCenter,
-        AlignLeft: AlignLeft,
-        AlignRight: AlignRight,
-        BackgroundImage: BackgroundImage,
-        GradientBackground: GradientBackground,
-        PlainBackground: PlainBackground,
-        WithButton: WithButton,
-        WithImage: WithImage,
-        WithImportedImage: WithImportedImage,
-        WithImportedBackground: WithImportedBackground,
-    },
+    Basic: BasicContainer,
+    Align: AlignContainer,
+    Background: BackgroundContainer,
+    BackgroundImage: BackgroundImageContainer,
+    WithButton: WithButtonContainer,
+    WithImage: WithImageContainer,
 }; 
