68313a8ff2cc08cefa3e2b600b25f692e13e33b4
nookery <nooks@qq.com>
2025-08-07 10:56:27 +0800

    🆕 Feature: 优化图片组件功能及文档

--- packages/cosy-ui/src-astro/types/image.ts
+++ packages/cosy-ui/src-astro/types/image.ts
@@ -2,15 +2,15 @@
  * 图片元数据接口
  * 用于统一管理项目中的图片元数据类型
  */
-export interface ImageMetadata {
-  src: string;
-  width: number;
-  height: number;
-  format: string;
+export interface IImageMetadata {
+    src: string;
+    width: number;
+    height: number;
+    format: string;
 }
 
 /**
  * 图片源类型
- * 可以是 ImageMetadata 对象或字符串 URL
+ * 可以是 IImageMetadata 对象或字符串 URL
  */
-export type ImageSource = ImageMetadata | string;
+export type ImageSource = IImageMetadata | string;
