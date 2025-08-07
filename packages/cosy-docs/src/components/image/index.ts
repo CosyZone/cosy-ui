57c8e56cb16fc66f8d6019cbde3fba3a43e887f3
nookery <nooks@qq.com>
2025-08-07 11:42:37 +0800

    🆕 Feature: Update image component with new functionality and docs

--- packages/cosy-docs/src/components/image/index.ts
+++ packages/cosy-docs/src/components/image/index.ts
@@ -1,10 +1,3 @@
-import ImageBasic from './ImageBasic.astro';
-import ImageRoundedShadow from './ImageRoundedShadow.astro';
-import ImageHover from './ImageHover.astro';
-import ImageLoadingIndicatorSpinner from './ImageLoadingIndicatorSpinner.astro';
-import ImageLoadingIndicatorProgress from './ImageLoadingIndicatorProgress.astro';
-import ImageObjectFit from './ImageObjectFit.astro';
-import ImageTransition from './ImageTransition.astro';
 
 import ImageBasicContainer from './ImageBasicContainer.astro';
 import ImageRoundedShadowContainer from './ImageRoundedShadowContainer.astro';
@@ -15,22 +8,11 @@ import ImageObjectFitContainer from './ImageObjectFitContainer.astro';
 import ImageTransitionContainer from './ImageTransitionContainer.astro';
 
 export const ComponentPackage = {
-    ComponentContainers: {
-        Basic: ImageBasicContainer,
-        RoundedShadow: ImageRoundedShadowContainer,
-        Hover: ImageHoverContainer,
-        LoadingIndicatorSpinner: ImageLoadingIndicatorSpinnerContainer,
-        LoadingIndicatorProgress: ImageLoadingIndicatorProgressContainer,
-        ObjectFit: ImageObjectFitContainer,
-        Transition: ImageTransitionContainer,
-    },
-    Examples: {
-        Basic: ImageBasic,
-        RoundedShadow: ImageRoundedShadow,
-        Hover: ImageHover,
-        LoadingIndicatorSpinner: ImageLoadingIndicatorSpinner,
-        LoadingIndicatorProgress: ImageLoadingIndicatorProgress,
-        ObjectFit: ImageObjectFit,
-        Transition: ImageTransition,
-    },
+    Basic: ImageBasicContainer,
+    RoundedShadow: ImageRoundedShadowContainer,
+    Hover: ImageHoverContainer,
+    LoadingIndicatorSpinner: ImageLoadingIndicatorSpinnerContainer,
+    LoadingIndicatorProgress: ImageLoadingIndicatorProgressContainer,
+    ObjectFit: ImageObjectFitContainer,
+    Transition: ImageTransitionContainer,
 }; 
--- packages/cosy-ui/src-astro/image/image.ts
+++ packages/cosy-ui/src-astro/image/image.ts
@@ -1,16 +1,16 @@
+import type { ImageMetadata } from 'astro';
+
 /**
  * 图片元数据接口
  * 用于统一管理项目中的图片元数据类型
+ * 继承自 Astro 的 ImageMetadata 类型
  */
-export interface IImageMetadata {
-    src: string;
-    width: number;
-    height: number;
-    format: string;
+export interface IImageMetadata extends ImageMetadata {
+    // 可以在这里添加额外的属性
 }
 
 /**
  * 图片源类型
- * 可以是 IImageMetadata 对象或字符串 URL
+ * 可以是 IImageMetadata 对象、字符串 URL 或 Astro 的 ImageMetadata
  */
-export type ImageSource = IImageMetadata | string;
+export type ImageSource = IImageMetadata | ImageMetadata | string;
