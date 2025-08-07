e8ff3f60cb7dfb62f4d3ccccc186a6bce394e077
Nookery <nooks@qq.com>
2025-08-07 22:38:07 +0800

    🗑️ Trash: Remove unused module and speak components

--- packages/cosy-ui/src-astro/module/index.ts
+++ packages/cosy-ui/src-astro/module/index.ts
@@ -1,17 +1,3 @@
 import Module from './Module.astro';
-import ModuleBasic from './ModuleBasic.astro';
-import ModuleGrid from './ModuleGrid.astro';
-import ModuleCustom from './ModuleCustom.astro';
-import BasicSourceCode from './ModuleBasic.astro?raw';
-import GridSourceCode from './ModuleGrid.astro?raw';
-import CustomSourceCode from './ModuleCustom.astro?raw';
-import { extractSimpleExample } from '../../src/utils/component';
 
-export { Module, ModuleBasic, ModuleGrid, ModuleCustom };
-
-// 导出示例源代码
-export const ModuleExampleCodes = {
-  Basic: extractSimpleExample(BasicSourceCode, 'Module'),
-  Grid: extractSimpleExample(GridSourceCode, 'Module'),
-  Custom: extractSimpleExample(CustomSourceCode, 'Module'),
-};
+export { Module };
