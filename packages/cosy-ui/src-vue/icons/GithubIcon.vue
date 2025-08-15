4687f3add60cc49923786afee9c396ab5f346489
Nookery <nooks@qq.com>
2025-08-15 23:40:24 +0800

    🆕 Feature: Add multiple new icons and update icon showcase components

---
+++ packages/cosy-ui/src-vue/icons/GithubIcon.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import Icon from './VueIcon.vue';
+
+interface Props {
+    /**
+     * 图标的大小
+     * @default "24px"
+     */
+    size?: string;
+    /**
+     * 自定义类名
+     */
+    class?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    size: '24px',
+    class: '',
+});
+</script>
+
+<template>
+    <Icon name="github" :size="size" :class="props.class" />
+</template>
---
+++ packages/cosy-ui/src-vue/icons/SocialIcon.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import Icon from './VueIcon.vue';
+
+interface Props {
+    /**
+     * 图标的大小
+     * @default "24px"
+     */
+    size?: string;
+    /**
+     * 图标的颜色
+     * @default "currentColor"
+     */
+    color?: string;
+    /**
+     * 自定义类名
+     */
+    class?: string;
+    /**
+     * 社交平台
+     */
+    platform: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    size: '24px',
+    color: 'currentColor',
+    class: '',
+});
+</script>
+
+<template>
+    <Icon :name="`social-${platform}`" :size="size" :color="color" :class="props.class" />
+</template>
