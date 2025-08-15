1175387da05bccbbf0abed215c074e055c296b0c
Nookery <nooks@qq.com>
2025-08-15 22:51:32 +0800

    🗑️ Trash: Remove deprecated resources docs and move icons to components

---
+++ packages/cosy-docs/src/components/icons/index.ts
@@ -0,0 +1,13 @@
+import IconShowcase from './IconShowcase.astro';
+import ClassContainer from './ClassContainer.astro';
+import ColorContainer from './ColorContainer.astro';
+import SizeContainer from './SizeContainer.astro';
+
+export const IconsPackage = {
+    IconShowcase,
+    ClassContainer,
+    ColorContainer,
+    SizeContainer,
+};
+
+export default IconsPackage; 
