690b9bc188ac11df791d7587d1b1845574b0e209
Nookery <nooks@qq.com>
2025-08-16 13:33:49 +0800

    💄 UI: Update AlertDialog component and related docs

---
+++ packages/cosy-docs/src/components/alert-dialog-vue/index.ts
@@ -0,0 +1,9 @@
+import AlertDialogVueBasicContainer from './AlertDialogVueBasicContainer.astro';
+import AlertDialogVueEnglishContainer from './AlertDialogVueEnglishContainer.astro';
+import AlertDialogVueCustomStyleContainer from './AlertDialogVueCustomStyleContainer.astro';
+
+export const AlertDialogVuePackage = {
+    Basic: AlertDialogVueBasicContainer,
+    English: AlertDialogVueEnglishContainer,
+    CustomStyle: AlertDialogVueCustomStyleContainer,
+};
