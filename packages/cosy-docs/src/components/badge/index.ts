4d1f30522a8d3e21594702d01df04c704142ffe7
Nookery <nooks@qq.com>
2025-08-16 13:01:39 +0800

    🆕 Feature: Add new Badge component variants and update documentation

--- packages/cosy-docs/src/components/badge/index.ts
+++ packages/cosy-docs/src/components/badge/index.ts
@@ -1,11 +1,15 @@
 import BadgeBasicContainer from './BadgeBasicContainer.astro';
+import BadgeClassContainer from './BadgeClassContainer.astro';
+import BadgeVariantContainer from './BadgeVariantContainer.astro';
 import OutlineContainer from './OutlineContainer.astro';
 import SizesContainer from './SizesContainer.astro';
 import VueContainer from './VueContainer.astro';
 
 export const BadgePackage = {
     Basic: BadgeBasicContainer,
+    Class: BadgeClassContainer,
     Outline: OutlineContainer,
     Sizes: SizesContainer,
+    Variant: BadgeVariantContainer,
     Vue: VueContainer,
 }; 
