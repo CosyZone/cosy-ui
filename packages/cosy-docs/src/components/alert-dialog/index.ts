73b4ccab03c9c175f53b3a4f0f5e12616153a81a
Nookery <nooks@qq.com>
2025-08-16 13:26:30 +0800

    🆕 Feature: Add custom style support for AlertDialog component

--- packages/cosy-docs/src/components/alert-dialog/index.ts
+++ packages/cosy-docs/src/components/alert-dialog/index.ts
@@ -1,7 +1,9 @@
 import BasicContainer from './BasicContainer.astro';
 import EnglishContainer from './EnglishContainer.astro';
+import CustomStyleContainer from './CustomStyleContainer.astro';
 
 export const AlertDialogPackage = {
     Basic: BasicContainer,
     English: EnglishContainer,
+    CustomStyle: CustomStyleContainer,
 }; 
