61843da359d5238708246d64f79556c52e363d1f
Nookery <nooks@qq.com>
2025-08-17 10:37:29 +0800

    🆕 Feature: Add Vue alert components with closable and type variants

---
+++ packages/cosy-docs/src/components/alert-vue/AlertVueClosableFalse.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { Alert } from '@coffic/cosy-ui/vue';
+
+/**
+ * @component AlertVueClosableFalse
+ * @description 展示 AlertVue 组件的不可关闭功能。
+ */
+</script>
+
+<template>
+    <Alert type="warning" :closable="false">
+        这是不可关闭的提示信息
+    </Alert>
+</template>
---
+++ packages/cosy-docs/src/components/alert-vue/AlertVueTypesWarning.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Alert } from '@coffic/cosy-ui/vue';
+
+/**
+ * @component AlertVueTypesWarning
+ * @description 展示 AlertVue 组件的警告类型。
+ */
+</script>
+
+<template>
+    <Alert type="warning" title="警告">这是一个警告提示</Alert>
+</template>
