084c8ea50649205e9f64611a40e9629cfc12cf09
Nookery <nooks@qq.com>
2025-08-17 12:35:29 +0800

    🆕 Feature: Add Vue implementation for mac-window component

---
+++ packages/cosy-docs/src/components/mac-window-vue/index.ts
@@ -0,0 +1,21 @@
+// Vue 容器组件
+import BasicVueContainer from './BasicVueContainer.astro';
+import WithTabsVueContainer from './WithTabsVueContainer.astro';
+import WithEventsVueContainer from './WithEventsVueContainer.astro';
+import WithToolbarVueContainer from './WithToolbarVueContainer.astro';
+import WithSidebarVueContainer from './WithSidebarVueContainer.astro';
+import CustomHeightVueContainer from './CustomHeightVueContainer.astro';
+import BgTypesVueContainer from './BgTypesVueContainer.astro';
+
+// 导出 MacWindow Vue 组件包
+export const MacWindowVuePackage = {
+    Containers: {
+        Basic: BasicVueContainer,
+        WithTabs: WithTabsVueContainer,
+        WithEvents: WithEventsVueContainer,
+        WithToolbar: WithToolbarVueContainer,
+        WithSidebar: WithSidebarVueContainer,
+        CustomHeight: CustomHeightVueContainer,
+        BgTypes: BgTypesVueContainer,
+    }
+};
