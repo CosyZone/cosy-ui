3f7a6ba4482c58b7e7799bca3d944725e89dd201
Nookery <nooks@qq.com>
2025-09-12 20:53:17 +0800

    🗑️ Trash: Remove deprecated ProductHero component files

--- packages/cosy-docs/src/components/product-hero/index.ts
+++ packages/cosy-docs/src/components/product-hero/index.ts
@@ -2,15 +2,9 @@ import ProductHeroExampleContainer from './ProductHeroExampleContainer.astro';
 import ProductHeroNameContainer from './ProductHeroNameContainer.astro';
 import ProductHeroDescriptionContainer from './ProductHeroDescriptionContainer.astro';
 import ProductHeroImageContainer from './ProductHeroImageContainer.astro';
-import ProductHeroPriceContainer from './ProductHeroPriceContainer.astro';
-import ProductHeroIsNewContainer from './ProductHeroIsNewContainer.astro';
+import ProductHeroHighlightContainer from './ProductHeroHighlightContainer.astro';
 import ProductHeroAlignContainer from './ProductHeroAlignContainer.astro';
 import ProductHeroBackgroundContainer from './ProductHeroBackgroundContainer.astro';
-import ProductHeroBuyNowTextContainer from './ProductHeroBuyNowTextContainer.astro';
-import ProductHeroLearnMoreTextContainer from './ProductHeroLearnMoreTextContainer.astro';
-import ProductHeroNewLabelTextContainer from './ProductHeroNewLabelTextContainer.astro';
-import ProductHeroOnBuyNowContainer from './ProductHeroOnBuyNowContainer.astro';
-import ProductHeroOnLearnMoreContainer from './ProductHeroOnLearnMoreContainer.astro';
 import ProductHeroClassContainer from './ProductHeroClassContainer.astro';
 import ProductHeroLabelsContainer from './ProductHeroLabelsContainer.astro';
 
@@ -19,11 +13,9 @@ export const ProductHeroPackage = {
     Name: ProductHeroNameContainer,
     Description: ProductHeroDescriptionContainer,
     Image: ProductHeroImageContainer,
-    Price: ProductHeroPriceContainer,
-    // IsNew 已废弃
+    Highlight: ProductHeroHighlightContainer,
     Align: ProductHeroAlignContainer,
     Background: ProductHeroBackgroundContainer,
-    // 以下已废弃：BuyNowText, LearnMoreText, NewLabelText, OnBuyNow, OnLearnMore
     Labels: ProductHeroLabelsContainer,
     Class: ProductHeroClassContainer,
 };
