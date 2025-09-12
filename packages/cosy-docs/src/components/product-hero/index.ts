812c7ac959407bae75cdc307a35040c818d7af65
Nookery <nooks@qq.com>
2025-09-12 20:16:52 +0800

    🗑️ Trash: Remove deprecated ProductHeroItem components and update docs

---
+++ packages/cosy-docs/src/components/product-hero/index.ts
@@ -0,0 +1,31 @@
+import ProductHeroExampleContainer from './ProductHeroExampleContainer.astro';
+import ProductHeroNameContainer from './ProductHeroNameContainer.astro';
+import ProductHeroDescriptionContainer from './ProductHeroDescriptionContainer.astro';
+import ProductHeroImageContainer from './ProductHeroImageContainer.astro';
+import ProductHeroPriceContainer from './ProductHeroPriceContainer.astro';
+import ProductHeroIsNewContainer from './ProductHeroIsNewContainer.astro';
+import ProductHeroAlignContainer from './ProductHeroAlignContainer.astro';
+import ProductHeroBackgroundContainer from './ProductHeroBackgroundContainer.astro';
+import ProductHeroBuyNowTextContainer from './ProductHeroBuyNowTextContainer.astro';
+import ProductHeroLearnMoreTextContainer from './ProductHeroLearnMoreTextContainer.astro';
+import ProductHeroNewLabelTextContainer from './ProductHeroNewLabelTextContainer.astro';
+import ProductHeroOnBuyNowContainer from './ProductHeroOnBuyNowContainer.astro';
+import ProductHeroOnLearnMoreContainer from './ProductHeroOnLearnMoreContainer.astro';
+import ProductHeroClassContainer from './ProductHeroClassContainer.astro';
+
+export const ProductHeroPackage = {
+    Example: ProductHeroExampleContainer,
+    Name: ProductHeroNameContainer,
+    Description: ProductHeroDescriptionContainer,
+    Image: ProductHeroImageContainer,
+    Price: ProductHeroPriceContainer,
+    IsNew: ProductHeroIsNewContainer,
+    Align: ProductHeroAlignContainer,
+    Background: ProductHeroBackgroundContainer,
+    BuyNowText: ProductHeroBuyNowTextContainer,
+    LearnMoreText: ProductHeroLearnMoreTextContainer,
+    NewLabelText: ProductHeroNewLabelTextContainer,
+    OnBuyNow: ProductHeroOnBuyNowContainer,
+    OnLearnMore: ProductHeroOnLearnMoreContainer,
+    Class: ProductHeroClassContainer,
+};
