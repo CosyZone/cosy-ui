3c04e6cc3ec10c4fffe1e52ffad3b069940e8059
Nookery <nooks@qq.com>
2025-09-13 10:22:29 +0800

    📖 Document: Add new component documentation for feature-card and feature-carousel

---
+++ packages/cosy-docs/src/components/feature-carousel/index.ts
@@ -0,0 +1,16 @@
+// FeatureCarousel 示例组件
+import FeatureCarouselBasic from './FeatureCarouselBasic.astro';
+
+// FeatureCarousel 容器组件
+import ClassContainer from './ClassContainer.astro';
+import FeaturesContainer from './FeaturesContainer.astro';
+
+export const FeatureCarouselPackage = {
+    FeatureCarouselExamples: {
+        Basic: FeatureCarouselBasic,
+    },
+    FeatureCarouselContainers: {
+        Class: ClassContainer,
+        Features: FeaturesContainer,
+    },
+};
