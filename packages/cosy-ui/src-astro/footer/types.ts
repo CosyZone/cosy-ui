1e317cd6edce66332c47c729b8a5e137722da773
nookery <nooks@qq.com>
2025-09-23 09:58:09 +0800

    💄 UI: Update footer component styling and demo pages

--- packages/cosy-ui/src-astro/footer/types.ts
+++ packages/cosy-ui/src-astro/footer/types.ts
@@ -5,7 +5,6 @@ export interface FooterICPProps {
 
 export interface FooterCopyrightProps {
     company: string;
-    copyright?: string;
     currentYear: number;
     t?: (key: string) => string;
 }
@@ -40,8 +39,6 @@ export interface FooterBrandProps {
 export interface IFooterProps {
     /** 公司名称 */
     company: string;
-    /** 版权信息 */
-    copyright: string;
     /** 背景色类型，支持所有预设背景色和透明度变体 */
     background?: BackgroundColor;
     /** 友情链接 */
