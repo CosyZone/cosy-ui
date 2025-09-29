f567fb72036b7ffb1079cea6de15ae59ce9b057b
nookery <nooks@qq.com>
2025-09-29 14:36:42 +0800

    🆕 Feature: Add border color showcase component

--- packages/cosy-docs/src/components/borders/index.ts
+++ packages/cosy-docs/src/components/borders/index.ts
@@ -1,5 +1,7 @@
 import BorderShowcase from "./BorderShowcase.astro";
+import BorderColorShowcase from "./BorderColorShowcase.astro";
 
 export const BorderPackage = {
-	BorderShowcase,
+    BorderShowcase,
+    BorderColorShowcase,
 };
