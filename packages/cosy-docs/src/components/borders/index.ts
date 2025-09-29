933f3b5fea175a4eb35b82bdbb348860a99a62f1
nookery <nooks@qq.com>
2025-09-29 14:36:56 +0800

    💄 UI: Update border showcase components and exports

--- packages/cosy-docs/src/components/borders/index.ts
+++ packages/cosy-docs/src/components/borders/index.ts
@@ -2,6 +2,6 @@ import BorderShowcase from "./BorderShowcase.astro";
 import BorderColorShowcase from "./BorderColorShowcase.astro";
 
 export const BorderPackage = {
-    BorderShowcase,
-    BorderColorShowcase,
+	BorderShowcase,
+	BorderColorShowcase,
 };
