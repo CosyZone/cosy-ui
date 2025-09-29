70f47a73455cff10dd85051439df941a477e7314
nookery <nooks@qq.com>
2025-09-29 14:02:13 +0800

    🆕 Feature: Add container border color variants and update documentation

--- packages/cosy-docs/src/components/container/index.ts
+++ packages/cosy-docs/src/components/container/index.ts
@@ -3,6 +3,7 @@ import ContainerBackgroundContainer from "./ContainerBackgroundContainer.astro";
 import ContainerBackgroundImageContainer from "./ContainerBackgroundImageContainer.astro";
 import ContainerBasicContainer from "./ContainerBasicContainer.astro";
 import ContainerBorderContainer from "./ContainerBorderContainer.astro";
+import ContainerBorderColorContainer from "./ContainerBorderColorContainer.astro";
 import ContainerCenteredContainer from "./ContainerCenteredContainer.astro";
 import ContainerClassListContainer from "./ContainerClassListContainer.astro";
 import ContainerCustomClassContainer from "./ContainerCustomClassContainer.astro";
@@ -26,34 +27,35 @@ import ContainerPaddingLeftContainer from "./ContainerPaddingLeftContainer.astro
 import ContainerPaddingRightContainer from "./ContainerPaddingRightContainer.astro";
 
 export const ContainerPackage = {
-	Examples: {
-		Basic: ContainerBasicContainer,
-	},
-	Containers: {
-		AspectRatio: ContainerAspectRatioContainer,
-		Background: ContainerBackgroundContainer,
-		BackgroundImage: ContainerBackgroundImageContainer,
-		Border: ContainerBorderContainer,
-		Centered: ContainerCenteredContainer,
-		ClassList: ContainerClassListContainer,
-		CustomClass: ContainerCustomClassContainer,
-		Flex: ContainerFlexContainer,
-		Gap: ContainerGapContainer,
-		Height: ContainerHeightContainer,
-		Items: ContainerItemsContainer,
-		Justify: ContainerJustifyContainer,
-		Margin: ContainerMarginContainer,
-		Padding: ContainerPaddingContainer,
-		PaddingAxis: ContainerPaddingAxisContainer,
-		PaddingTop: ContainerPaddingTopContainer,
-		PaddingBottom: ContainerPaddingBottomContainer,
-		PaddingX: ContainerPaddingXContainer,
-		PaddingLeft: ContainerPaddingLeftContainer,
-		PaddingRight: ContainerPaddingRightContainer,
-		Rounded: ContainerRoundedContainer,
-		Widths: ContainerWidthsContainer,
-		Fit: ContainerFitContainer,
-		ContentBorder: ContainerContentBorderContainer,
-		Slot: ContainerSlotContainer,
-	},
+    Examples: {
+        Basic: ContainerBasicContainer,
+    },
+    Containers: {
+        AspectRatio: ContainerAspectRatioContainer,
+        Background: ContainerBackgroundContainer,
+        BackgroundImage: ContainerBackgroundImageContainer,
+        Border: ContainerBorderContainer,
+        BorderColor: ContainerBorderColorContainer,
+        Centered: ContainerCenteredContainer,
+        ClassList: ContainerClassListContainer,
+        CustomClass: ContainerCustomClassContainer,
+        Flex: ContainerFlexContainer,
+        Gap: ContainerGapContainer,
+        Height: ContainerHeightContainer,
+        Items: ContainerItemsContainer,
+        Justify: ContainerJustifyContainer,
+        Margin: ContainerMarginContainer,
+        Padding: ContainerPaddingContainer,
+        PaddingAxis: ContainerPaddingAxisContainer,
+        PaddingTop: ContainerPaddingTopContainer,
+        PaddingBottom: ContainerPaddingBottomContainer,
+        PaddingX: ContainerPaddingXContainer,
+        PaddingLeft: ContainerPaddingLeftContainer,
+        PaddingRight: ContainerPaddingRightContainer,
+        Rounded: ContainerRoundedContainer,
+        Widths: ContainerWidthsContainer,
+        Fit: ContainerFitContainer,
+        ContentBorder: ContainerContentBorderContainer,
+        Slot: ContainerSlotContainer,
+    },
 };
