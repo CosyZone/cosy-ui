6ada56759567593d8db47f4cb24151d21ebe5f9d
Nookery <nooks@qq.com>
2025-10-12 21:18:09 +0800

    🎨 Chore: Update container component exports

--- packages/cosy-ui/src-vue/container/index.ts
+++ packages/cosy-ui/src-vue/container/index.ts
@@ -1,11 +1,1 @@
 export { default as Container } from "./Container.vue";
-export { default as ContainerBg } from "./ContainerBg.vue";
-export { default as ContainerError } from "./ContainerError.vue";
-export { default as AspectRatioBox } from "./AspectRatioBox.vue";
-export { default as DefaultAspectRatioBox } from "./DefaultAspectRatioBox.vue";
-export { default as ExplicitHeightAspectRatioBox } from "./ExplicitHeightAspectRatioBox.vue";
-
-// 验证函数
-export { validateContainer } from "./validate";
-export { validatePaddingConflict } from "../../src/components/container/validate-padding";
-export { validateSizing } from "../../src/components/container/validate-sizing";
