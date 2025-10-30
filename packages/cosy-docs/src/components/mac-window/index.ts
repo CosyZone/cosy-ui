5189e50960bafa688bf0b1533a12e73f591faf60
Nookery <nooks@qq.com>
2025-10-30 21:26:26 +0800

    🎨 Chore: 重构mac-window组件，移除vue版本并整合功能

--- packages/cosy-docs/src/components/mac-window/index.ts
+++ packages/cosy-docs/src/components/mac-window/index.ts
@@ -1,4 +1,4 @@
-// Astro 容器组件
+// 容器组件（支持 Astro 和 Vue）
 import BasicContainer from "./BasicContainer.astro";
 import BgTypesContainer from "./BgTypesContainer.astro";
 import CustomHeightContainer from "./CustomHeightContainer.astro";
@@ -10,14 +10,14 @@ import WithToolbarContainer from "./WithToolbarContainer.astro";
 
 // 导出 MacWindow 组件包
 export const MacWindowPackage = {
-	Containers: {
-		Basic: BasicContainer,
-		WithTabs: WithTabsContainer,
-		WithEvents: WithEventsContainer,
-		WithToolbar: WithToolbarContainer,
-		WithSidebar: WithSidebarContainer,
-		CustomHeight: CustomHeightContainer,
-		CustomWidth: CustomWidthContainer,
-		BgTypes: BgTypesContainer,
-	},
+    Containers: {
+        Basic: BasicContainer,
+        WithTabs: WithTabsContainer,
+        WithEvents: WithEventsContainer,
+        WithToolbar: WithToolbarContainer,
+        WithSidebar: WithSidebarContainer,
+        CustomHeight: CustomHeightContainer,
+        CustomWidth: CustomWidthContainer,
+        BgTypes: BgTypesContainer,
+    },
 };
