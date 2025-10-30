60503c318b92b0ea5f09ff1c6c88888c8d6bcffd
Nookery <nooks@qq.com>
2025-10-30 20:34:18 +0800

    🎨 Chore: Consolidate alert-dialog Vue components into main directory

--- packages/cosy-docs/src/components/alert-dialog/CustomStyleVue.vue
+++ packages/cosy-docs/src/components/alert-dialog/CustomStyleVue.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
-import { AlertDialog } from "@coffic/cosy-ui/vue";
-import { ref } from "vue";
+import { AlertDialog } from '@coffic/cosy-ui/vue';
+import { ref } from 'vue';
 
 const showDialog = ref(false);
 </script>
-
 <template>
-    <div style="display: flex; flex-direction: column; gap: 1rem;">
+    <div style="display: flex; flex-direction: column; gap: 1rem">
         <button @click="showDialog = true" class="cosy:btn cosy:btn-primary">
             显示自定义样式对话框
         </button>
